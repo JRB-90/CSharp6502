8770fc579e0c808257343f6ef819cd44ec1ce32a
Jacob Brown <Jake@insphereltd.com>
2021-11-11 20:02:00 +0000

    Added the core project

---
+++ CS6502.Core/ICpu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS6502.Core
+{
+    /// <summary>
+    /// Interface defining the behaviour of all 6502 cpu variants.
+    /// </summary>
+    public interface ICpu
+    {
+    }
+}
